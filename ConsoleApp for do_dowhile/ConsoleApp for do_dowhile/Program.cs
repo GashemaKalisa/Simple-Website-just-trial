83a7854347242bc6e0fa0e45b6dc109d0951ee3b
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-11 08:40:50 -0500

    do with while loop

---
+++ ConsoleApp for do_dowhile/ConsoleApp for do_dowhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ConsoleApp_for_do_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //starting of do while loop
+            int num = 1;
+            do
+            {
+                Console.WriteLine("is in the loop");
+                num++;//increament of variable num
+            }
+            while (num < 5);
+
+            //while loop
+            int numb = 1;
+            while(numb < 3)
+            {
+                Console.WriteLine("counting numbers");
+                numb++;//increament of variable numb
+            }
+            Console.ReadLine();
+        }
+    }
+}
