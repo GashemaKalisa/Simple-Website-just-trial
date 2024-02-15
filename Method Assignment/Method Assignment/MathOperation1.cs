b3a7785e262d4eb2699a5e1b5067338fa19fdb19
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-15 17:27:21 -0500

    Method Assignment

---
+++ Method Assignment/Method Assignment/MathOperation1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class MathOperation1
+    {
+        //crate a method that takes two intergers as parameters
+        //Make on parameter optional by providing a default value
+        public int MathOperation(int i, int j = 1)
+        {
+            int result = i * j;
+            return result;
+        }
+    }
+}
