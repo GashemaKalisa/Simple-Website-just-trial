d1452d9fa4bd976a2e08ff23a46bf98454302437
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-16 13:44:12 -0500

    Method Assignment

---
+++ Assignment Submission/Assignment Submission/Mathoperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Submission
+{
+    class Mathoperation
+    {
+        //crate a method that takes two intergers as parameters
+        //Make on parameter optional by providing a default value
+        public int Calculation(int i, int j)
+        {
+            int product = i * j;
+            //int sum = i + j;
+            //int Quatient = i / j;
+            //int different = i - j;
+            return product;
+            //return sum;
+            //return Quatient;
+            //return different;
+        }
+    }
+}
---
+++ Assignment Submission/Assignment Submission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate MathOperation1 class
+            Mathoperation numbers = new Mathoperation();
+            //Ask the user to provide first parameter
+            Console.WriteLine("Enter an integer: ");
+            int first_number = Convert.ToInt32(Console.ReadLine());
+            //Ask the user to provide Second parameter
+            Console.WriteLine("Enter an integer: ");
+            int Second_number = Convert.ToInt32(Console.ReadLine());
+        
+            int product = numbers.Calculation(first_number, Second_number);
+            Console.WriteLine(product);
+            
+            
+        }
+    }
+}
