c6d9058df09b772b034e5feeda219bc32afba722
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-16 17:09:03 -0500

    Update Program.cs

--- Assignment Submission/Assignment Submission/Program.cs
+++ Assignment Submission/Assignment Submission/Program.cs
@@ -12,15 +12,12 @@ namespace Assignment_Submission
         {
             //Instantiate MathOperation1 class
             Mathoperation numbers = new Mathoperation();
-            //Ask the user to provide first parameter
-            Console.WriteLine("Enter a first number: ");
-            int first_number = Convert.ToInt32(Console.ReadLine());
-            //Ask the user to provide Second parameter
-            Console.WriteLine("Enter a second number: ");
-            int Second_number = Convert.ToInt32(Console.ReadLine());
-        
-            int product = numbers.Calculation(first_number, Second_number);
-            Console.WriteLine(product);
+          //Method that display second integer
+            Console.WriteLine("The scond number: ");
+            
+            
+            Console.WriteLine("The Product is: "+numbers.Calculation());
+            Console.ReadLine();
             
             
         }
