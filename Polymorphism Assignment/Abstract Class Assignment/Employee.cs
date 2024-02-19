56ceb326f0e40d48f500e161694f911c821736e1
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-19 09:53:50 -0500

    Need debuging
    It's is an assignment but not yet submitted

---
+++ Polymorphism Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    //Interface inheritance for the class Employee
+    public class Employee : IQuittable
+    {
+        public int Id { get; set; }
+      
+        //Implement the Quit() method inside of the Employee class
+        public void Quit()
+        {
+            Console.WriteLine("He live in"+ Country+ " in the city of "+ city);
+        }
+           
+        
+    }
+}
---
+++ Polymorphism Assignment/Abstract Class Assignment/IQuittable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    //Interface Name
+    interface IQuittable
+    {
+        string Country { get; set; }
+        
+         string city { get; set; }
+
+        //Method name
+         void Quit();
+    }
+}
