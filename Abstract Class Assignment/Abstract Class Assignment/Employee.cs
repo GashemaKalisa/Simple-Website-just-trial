86403d6dade29bce3d60573ca84f61ea55cf0744
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-19 08:37:17 -0500

    Creation of Abstract class

---
+++ Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public class Employee :Person
+    {
+        public int Id { get; set; }
+        //Implement the SayName() method inside of the Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+
+        }
+    }
+}
---
+++ Abstract Class Assignment/Abstract Class Assignment/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    //Create an abstract class "Person" with two string properties : FirstName and LastName
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //Give it the method SayName().
+        public abstract void SayName();
+    }
+}
+
+
