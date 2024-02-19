478fe35301adcd6d2b506035b7926640759c0fc6
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-19 12:22:26 -0500

    Update Person.cs

--- Polymorphism Assignment/Abstract Class Assignment/Person.cs
+++ Polymorphism Assignment/Abstract Class Assignment/Person.cs
@@ -9,10 +9,10 @@ namespace Abstract_Class_Assignment
     //Create an abstract class "Person" with two string properties : FirstName and LastName
     public abstract class Person
     {
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        //Give it the method SayName().
-        public abstract void SayName();
+        public string Country { get; set; }
+        public string city { get; set; }
+        //Give it the method Quit().
+        public  void Quit();
     }
 }
 
