0bb52963de3917f192d14a51b8566baab9ee40fd
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-20 12:05:05 -0500

    need to debug

---
+++ Operator Assignment/Operator Assignment/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Assignment
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int Id2 { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Employee()
+        {
+            this.Id = 001;
+            this.Id2 = 002;
+        }
+
+        public static bool opera == (Employee Id, Employee Id2)
+            
+            {
+            if(Id== Id2){
+            return true;
+            
+             }
+            else{
+            return false;
+            }
+        public static bool opera !=(Id, Id2)
+            {
+            if(Id!=Id2){
+            return false;
+            }
+            else{
+            return true;
+            }
+             
+	{
+
+	}
+
+        }
+        
+        
+    
+   
+
---
+++ Operator Assignment/Operator Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //, instaiate an Employee object
+            Employee comparison = new Employee() { Id = 001, FirstName = "Gashema", LastName="Kalisa" };
+            Employee comparison_s = new Employee() { Id2 = 002, FirstName = "Hazard", LastName = "stanley" };
+
+            if (Id == Id2)
+            {
+                Console.WriteLine("id are equal");
+            }
+            else
+            {
+                Console.WriteLine("Id are not equal");
+            }
+            Console.ReadLine();
+          
+        }
+        
+    }
+}
