a0bfcf117a9f99759c7ee21f77aa137a2213de05
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-22 17:30:45 -0500

    Update Program.cs

--- Operator Assignment/Operator Assignment/Program.cs
+++ Operator Assignment/Operator Assignment/Program.cs
@@ -11,8 +11,32 @@ namespace Operator_Assignment
         static void Main(string[] args)
         {
             //, instaiate an Employee object
-            Employee comparison = new Employee();
-           
+            var compar = new Employee();
+
+            //Assigning Values to the properties
+            compar.Id = 001;
+            compar.FirstName = "Gashema";
+            compar.LastName = "Kalisa";
+
+            var comparis = new Employee();
+            comparis.Id = 002;
+            comparis.FirstName = "Hazard";
+            comparis.LastName = "Stanley";
+
+            if (compar == comparis)
+            {
+                Console.WriteLine("Employee " + compar.LastName + " his Idd  equal to " + comparis.LastName);
+            }
+            compar.Id = 001;
+
+
+
+            if (compar != comparis)
+            {
+                Console.WriteLine("Employee " + compar.LastName + " his Idd not equal to " + comparis.LastName);
+            }
+            compar.Id = 002;
+
             Console.ReadLine();
           
         }
