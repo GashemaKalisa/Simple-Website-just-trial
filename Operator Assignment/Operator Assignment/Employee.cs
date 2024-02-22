54b59874a4b1fe58b71c66f24cc963d91a7ea11c
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-02-22 17:29:54 -0500

    Update Employee.cs

--- Operator Assignment/Operator Assignment/Employee.cs
+++ Operator Assignment/Operator Assignment/Employee.cs
@@ -6,89 +6,33 @@ using System.Threading.Tasks;
 
 namespace Operator_Assignment
 {
+    //Creation of Employee class with Id firstname and lastname Properites
     class Employee
     {
-        int Id1, Id2;
-        string Firstname, Lastname;
-        public Employee()
+        public int Id { get; set; }
+        //public int Id2 { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //Declaration of boolean
+        public static bool operator == (Employee Id1,Employee Id2)
         {
-            this.Id1 = 001;
-            this.Id2 = 002;
-            this.Firstname = "Gashema";
-            this.Lastname = "Kalisa";
-
-        }
-        public Employee(int Id1, int Id2,string FirstNames,string LastName)
-        {
-            this.Id1 = Id1;
-            this.Id2 = Id2;
-            //this.FirstNames = FirstNames;
-            //this.LastName = LastName;
-        }
-        public bool Operator  ( int Id1, int Id2)
-        {
-            if(Id1 == Id2) 
+            if (Id1.Id== Id2.Id)//condition in equality
             {
-                Console.WriteLine("First ID is equal to Second Id");
-                Console.WriteLine(Firstname);
                 return true;
             }
-            else
+            return false;
+        }
+
+        public static bool operator !=(Employee Id1, Employee Id2)//declariton of ineqality 
+        {
+            if (Id1.Id == Id2.Id)
             {
                 return false;
             }
-           
+            return true;
         }
-        public bool Operator(int Id1, int Id2)
-        {
-            if (Id1 != Id2)
-            {
-                Console.WriteLine("First ID is not equal to Second Id");
-                Console.WriteLine(Lastname);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
-        }
-
     }
- //       public int ope Id { get; set; }
- //       public int Id2 { get; set; }
- //       public string FirstName { get; set; }
- //       public string LastName { get; set; }
- //       public Employee()
- //       {
- //           this.Id = 001;
- //           this.Id2 = 002;
- //       }
-
- //       public static bool opera == (Employee Id, Employee Id2)
-            
- //           {
- //           if(Id== Id2){
- //           return true;
-            
- //            }
- //           else{
- //           return false;
- //           }
- //       public static bool opera !=(Id, Id2)
- //           {
- //           if(Id!=Id2){
- //           return false;
- //           }
- //           else{
- //           return true;
- //           }
-             
-	//{
-
-	//}
-
-        }
+}
         
         
     
